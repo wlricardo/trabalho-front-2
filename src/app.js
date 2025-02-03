791085570d8cfe74359e73fcc1b6ae8ed67802e6
wlricardo <wlricardo@gmail.com>
2025-02-02 22:14:50 -0300

    Testando o data biding

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 const myNameApp = {
   data() {
     return {
-      name: "Willem",
+      name: "",
+      age: 48,
+      input_name: "",
     };
   },
+  methods: {
+    submitForm(e) {
+      e.preventDefault();
+      console.log(this.input_name);
+      this.name = this.input_name;
+    },
+  },
 };
+
+Vue.createApp(myNameApp).mount("#app");
